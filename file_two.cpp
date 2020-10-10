4c96654719f9c99ebab70f7b60591ec7b718e802
hubieva-a <a.033518@gmail.com>
2020-10-10 23:41:36 +0300

    Commit number four

--- file_two.cpp
+++ file_two.cpp
@@ -1,10 +1,11 @@
 #include<iostream>
+#include<math.h>
 using namespace std;
 float A,r,R;
 int main() {
   cin>>A;
   r=A/(2*sqrt(3));
   R=A/sqrt(3);
-  cout<<"r="<<r<<" R="<<R;
+  cout<<"Hi everyone"<<"r="<<r<<" R="<<R;
   return 0;
 }
